8b5a5f3974b71a6158684c4786886ea2591bedf0
SAMID AMAURY BARRERA CAMARGO <sabarrera21@ucatolica.edu.co>
2024-01-20 12:08:06 -0500

    Configuracion  de detalle play list y css base

--- routers/detalleplaylist.py
+++ routers/detalleplaylist.py
@@ -6,27 +6,50 @@ from fastapi.templating import Jinja2Templates
 templates = Jinja2Templates(directory="templates")
 
 from models import detalleplaylis
-from models.detalleplaylis import DetallePlaylist
 
 router = APIRouter()
-@router.get(path="/{viejito}" , response_model=List[DetallePlaylist])
-async def list_Detalle():
-    return list(detalleplaylis.detalleplaylists)
 
-@router.put("/{detalleplaylis_id}")
-def insert_Detalle(
-        detalleplaylis_id: int=Path(...,
-        description="Id de detalle" ,
-        title="detalle",
-        gt=0),
-        detalleplaylisn: DetallePlaylist= Body(...)
-    ):
-    tmp2=detalleplaylis.detalleplaylists
-    tmp2.append(detalleplaylisn)
-    return detalleplaylisn
+@router.get("/home", response_class=HTMLResponse)
+def home_detalles(request: Request):
+    return templates.TemplateResponse("generic.html",
+    {"request": request,
+     "title": "Play lista de Usuario",
+     "texto": "Detalles de playlist de  Usuarios "
+     })
+
 @router.get("/",response_class=HTMLResponse)
-def show_detalles(request:Request):
-    return templates.TemplateResponse(
-        "detalleplaylists.html",
-        {"request": request, "title": "detalles", "detalles":
-            detalleplaylis.detalleplaylists})
+def show_detalle(request: Request):
+    return templates.TemplateResponse("detalleplaylists.html",{
+        "request": request ,
+        "title": "listas de Musica",
+        "detalleplaylists":detalleplaylis
+    })
+
+@router.get("/create", response_class=HTMLResponse,status_code=201)
+def create_Detalle(request: Request):
+    return templates.TemplateResponse("newPlaylistDetail.html",{
+        "request": request,
+        "title": "Nueva lista de Usuario"
+    })
+
+@router.post("/new", status_code=201)
+def add_Detalle( id: int =Form(...),id_playlist: int =Form(...),id_cancion: int=Form(...)):
+    body_detalle = {
+        "id":id, "id_playlist":id_playlist, "id_cancion":id_cancion
+    }
+    detalleplaylis.detalleplaylists.append(body_detalle)
+    return RedirectResponse(url="/detalles/",status_code=303)
+
+@router.get("/{id_detail}", response_class=HTMLResponse)
+def show_detalle(request: Request, id_detail : int = Path(...)):
+    detalle= detalleplaylis.detalleplaylists.__getitem__(id_detail)
+    response = templates.TemplateResponse("detalleplaylist.html",{
+        "request": request,
+        "title": "lista",
+        "detalleplaylist":detalle
+    })
+    if not detalle:
+        response.status_code=404
+    return response
+
+
